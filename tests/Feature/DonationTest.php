a168807bb88771971f24fa04ec54761794c327db
Daniil <dan-trofimov@mail.ru>
2021-05-18 23:35:49 +0300

    Add more tests for donations endpoint

--- tests/Feature/DonationTest.php
+++ tests/Feature/DonationTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\Donation;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\UserTestCase;
+use function Couchbase\fastlzCompress;
 
 class DonationTest extends UserTestCase
 {
@@ -21,13 +24,164 @@ class DonationTest extends UserTestCase
             ->assertExactJson([]);
     }
 
-    /**
-     * Tests todo:
-     * 1. Can retrieve one (simple)
-     * 2. Can retrieve sorted by amount
-     * 3. Can retrieve sorted by paid_at asc
-     * 4. Can retrieve where is_hidden=true
-     * 5. Can retrieve with skip+limit
-     */
+    /** @test */
+    public function canRetrieveDonation()
+    {
+        /** @var Donation $donation */
+        $donation = Donation::factory()->create([
+            'is_hidden' => false,
+        ])->refresh();
+
+        $data = [
+            'sort-field' => 'paid_at'
+        ];
+
+        $this->getJson(route('donations.index', $data))
+            ->assertStatus(200)
+            ->assertExactJson([
+                [
+                    'id' => $donation->id,
+                    'is_hidden' => $donation->is_hidden,
+                    'source' => $donation->source,
+                    'external_id' => $donation->external_id,
+                    'from' => $donation->from,
+                    'amount' => $donation->amount,
+                    'commission' => $donation->commission,
+                    'comment' => $donation->comment,
+                    'status' => $donation->status,
+                    'additional_data' => $donation->additional_data,
+                    'paid_at' => $donation->paid_at,
+                    'created_at' => $donation->created_at,
+                    'updated_at' => $donation->updated_at
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function getDonationsSortedByAmount()
+    {
+        /** @var Donation $donation */
+        $donation = Donation::factory()->create(['is_hidden' => false, 'amount' => 200]);
+        /** @var Donation $donation2 */
+        $donation2 = Donation::factory()->create(['is_hidden' => false, 'amount' => 300]);
+        /** @var Donation $donation3 */
+        $donation3 = Donation::factory()->create(['is_hidden' => false, 'amount' => 100]);
+
+        $data = [
+            'sort-field' => 'amount',
+            'sort-order' => 'asc'
+        ];
+
+        $this->getJson(route('donations.index', $data))
+            ->assertStatus(200)
+            ->assertJson([
+                [
+                    'id' => $donation3->id,
+                    'amount' => $donation3->amount
+                ], [
+                    'id' => $donation->id,
+                    'amount' => $donation->amount
+                ], [
+                    'id' => $donation2->id,
+                    'amount' => $donation2->amount
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function getDonationsSortedByPaidAt()
+    {
+        /** @var Donation $donation */
+        $donation = Donation::factory()
+            ->create(['is_hidden' => false, 'paid_at' => Carbon::now()->addMinute()])
+            ->refresh();
+        /** @var Donation $donation2 */
+        $donation2 = Donation::factory()
+            ->create(['is_hidden' => false, 'paid_at' => Carbon::now()->subMinute()])
+            ->refresh();
+        /** @var Donation $donation3 */
+        $donation3 = Donation::factory()
+            ->create(['is_hidden' => false, 'paid_at' => Carbon::now()])
+            ->refresh();
+
+        $data = [
+            'sort-field' => 'paid_at',
+            'sort-order' => 'asc'
+        ];
+
+        $this->getJson(route('donations.index', $data))
+            ->assertStatus(200)
+            ->assertJson([
+                [
+                    'id' => $donation2->id,
+                    'paid_at' => $donation2->paid_at
+                ], [
+                    'id' => $donation3->id,
+                    'paid_at' => $donation3->paid_at
+                ], [
+                    'id' => $donation->id,
+                    'paid_at' => $donation->paid_at
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function getHiddenDonations()
+    {
+        Donation::factory()->create(['is_hidden' => false]);
+        /** @var Donation $donation */
+        $donation = Donation::factory()->create(['is_hidden' => true, 'amount' => 500]);
+        Donation::factory()->create(['is_hidden' => false]);
+        /** @var Donation $donation2 */
+        $donation2 = Donation::factory()->create(['is_hidden' => true, 'amount' => 700]);
+
+        $data = [
+            'sort-field' => 'amount',
+            'sort-order' => 'desc',
+            'is-hidden' => true
+        ];
+
+        $this->getJson(route('donations.index', $data))
+            ->assertStatus(200)
+            ->assertJson([
+                [
+                    'id' => $donation2->id,
+                    'amount' => $donation2->amount
+                ], [
+                    'id' => $donation->id,
+                    'amount' => $donation->amount
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function getDonationsWithLimitAndSkippingSome()
+    {
+        $donations = Donation::factory(20)
+            ->create(['is_hidden' => false])
+            ->sortByDesc('amount')
+            ->values();
+
+        $data = [
+            'sort-field' => 'amount',
+            'limit' => 3,
+            'skip' => 11
+        ];
+
+        $this->getJson(route('donations.index', $data))
+            ->assertStatus(200)
+            ->assertJson([
+                [
+                    'id' => $donations[11]->id,
+                    'amount' => $donations[11]->amount
+                ], [
+                    'id' => $donations[12]->id,
+                    'amount' => $donations[12]->amount
+                ], [
+                    'id' => $donations[13]->id,
+                    'amount' => $donations[13]->amount
+                ]
+            ]);
+    }
 }
 
