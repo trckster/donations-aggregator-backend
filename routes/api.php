3ca0e4f6c77d514b35130d7af45f7a77b4f014b0
Daniil <dan-trofimov@mail.ru>
2021-06-10 22:15:01 +0300

    Protect api with auth

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,10 @@ Route::post('/login', [LoginController::class, 'login'])->name('login');
 
 Route::middleware('auth')->group(function () {
     Route::get('me', UserController::class)->name('me');
+
+    Route::prefix('donations')->name('donations.')->group(function () {
+        Route::get('', [DonationController::class, 'index'])->name('index');
+        Route::put('{donation}', [DonationController::class, 'update'])->name('update');
+    });
 });
 
-Route::prefix('donations')->name('donations.')->group(function () {
-    Route::get('', [DonationController::class, 'index'])->name('index');
-    Route::put('{donation}', [DonationController::class, 'update'])->name('update');
-});
